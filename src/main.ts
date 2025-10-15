34e3cf765447d553f9734f8f5e734caed852a179
Dominic-Um <dumbrasa@ucsc.edu>
2025-10-15 08:47:07 +0000

    Autoclicker, step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,27 @@ document.body.innerHTML = `
   </button>
     <div id="counterDisplay">0 paperclips</div>
 `;
+
+const button = document.getElementById("iconButton") as HTMLButtonElement;
+const counterDisplay = document.getElementById(
+  "counterDisplay",
+) as HTMLDivElement;
+
+function updateDisplay() {
+  counterDisplay.textContent = `${counter} paperclip${
+    counter === 1 ? "" : "s"
+  }`;
+}
+
+button.addEventListener("click", () => {
+  counter++;
+  counterDisplay.textContent = `${counter} paperclip${
+    counter === 1 ? "" : "s"
+  }`;
+});
+
+setInterval(() => {
+  counter++;
+  updateDisplay();
+}, 1000);
+updateDisplay();
