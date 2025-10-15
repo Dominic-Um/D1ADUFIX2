7e9b44dd9fae288b56474eb2609c697a91de3b48
Dominic-Um <dumbrasa@ucsc.edu>
2025-10-15 09:16:54 +0000

    Added counter and fixed button size.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,19 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-let  counter: number = 0;
+let counter: number = 0;
 
 document.body.innerHTML = `
-  <button id="iconButton">
+  <button id="iconButton" style="border:none; background:none; padding:0; cursor:pointer;">
     <img src="${exampleIconUrl}" alt="Paperclip Icon" width="100" />
   </button>
-    <div id="counterDisplay">0 paperclips</div>
+  <div id="counterDisplay">0 paperclips</div>
 `;
 
 const button = document.getElementById("iconButton") as HTMLButtonElement;
 const counterDisplay = document.getElementById(
   "counterDisplay",
 ) as HTMLDivElement;
-
-function updateDisplay() {
-  counterDisplay.textContent = `${counter} paperclip${
-    counter === 1 ? "" : "s"
-  }`;
-}
 
 button.addEventListener("click", () => {
   counter++;
