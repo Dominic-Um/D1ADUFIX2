67fc1c14bc432a71aa33032b18bd47b8b0a0e068
Dominic-Um <dumbrasa@ucsc.edu>
2025-10-15 08:46:09 +0000

    Fixed button size and added a counter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
   <button id="iconButton">
-    <img src="${exampleIconUrl}" alt="Paperclip Icon" />
+    <img src="${exampleIconUrl}" alt="Paperclip Icon" width="100" />
   </button>
+    <div id="counterDisplay">0 paperclips</div>
 `;
